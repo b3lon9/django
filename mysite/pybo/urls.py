b8f5343e5fd9e08e11a36571185d119fb11da625
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2022-12-02 20:34:17 +0900

    [jump to django] 02-06 데이터저장

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -3,6 +3,7 @@ from pybo import views
 
 app_name = 'pybo'
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail)
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name="answer_create"),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from pybo.models import Question
+from pybo.models import Question, Answer
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,10 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context=context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    a = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    a.save()
+    return redirect('pybo:detail', question_id=question.id)
