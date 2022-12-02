19b314302aba25ae0079b50f19dc2f1174c5fe1e
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2022-12-02 11:20:37 +0900

    [jump to django] 2-02 모델 2-03 장고관리자

--- mysite/pybo/admin.py
+++ mysite/pybo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from pybo.models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject', 'content']
 
 # Register your models here.
+admin.site.register(Question, QuestionAdmin)
--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# 질문(Question)
+class Question(models.Model):
+    # 제목
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+# 답변(Answer)
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
