2ef2e6c2853588cbee343a1c4ab34a6d3ce3d2e2
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2022-12-05 11:51:36 +0900

    [jump to django] 03-06 회원가입 03-07 모델 변경
    ...

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # 질문(Question)
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     # 제목
     subject = models.CharField(max_length=200)
     content = models.TextField()
@@ -13,6 +15,7 @@ class Question(models.Model):
 
 # 답변(Answer)
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,6 +4,7 @@ from pybo.models import Question, Answer
 from django.utils import timezone
 from pybo.forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -21,18 +22,21 @@ def detail(request, question_id):
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context=context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user    # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible')
+        form = AnswerForm()
+        # return HttpResponseNotAllowed('Only POST is possible')
     # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     # a = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
     # a.save()
@@ -40,11 +44,13 @@ def answer_create(request, question_id):
     # return redirect('pybo:detail', question_id=question.id)
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():         # 폼이 유효하다면 
             question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴받는다.
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
