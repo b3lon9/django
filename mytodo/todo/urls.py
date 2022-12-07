e80c6eaf6c1644085277937c10bbc87501b8ca14
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2022-12-07 11:21:21 +0900

    [TodoAPI] Todo 완료 API
    - 완료된 목록 불러오기
    - 완료할 목록 전달하기 > get으로 전달하면 done.complete=True로 전환 후 저장해주는 방식^^

--- mytodo/todo/urls.py
+++ mytodo/todo/urls.py
@@ -6,4 +6,6 @@ app_name = 'todo'
 urlpatterns = [
     path('todo/', views.TodosAPIView.as_view()),
     path('todo/<int:pk>/', views.TodoAPIView.as_view()),
+    path('done/', views.DoneTodosView.as_view()),
+    path('done/<int:pk>/', views.DoneTodoView.as_view()),
 ]
